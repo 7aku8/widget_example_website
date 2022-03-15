c4664a87a0b706a7a91f9e7fae3212f519334274
Jakub <jwolak@cryptovoucher.io>
2022-03-15 11:10:04 +0100

    new layout

---
+++ components/Widget.vue
@@ -0,0 +1,63 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <div id="widget_embed" />
+
+    <div class="flex flex-col items-center gap-2">
+      <h1 class="text-xl">Widget v2</h1>
+      <span class="font-thin text-sm text-gray-500">Choose integration type to trigger widget's flow...</span>
+      <div class="p-6 flex justify-center gap-6">
+        <button @click="checkout" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Checkout</button>
+        <button @click="referral" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Referral</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// widget install !
+import CryptoVoucherWidget from 'widget_embed_script';
+// import 'widget_embed_script/dist/style.css';
+import '../node_modules/widget_embed_script/dist/style.css'
+
+export default {
+  name: 'Widget',
+  methods: {
+    checkout() {
+      function uuid() {
+        const template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+        const xAndYOnly = /[xy]/g;
+
+        return template.replace(xAndYOnly, (character) => {
+          const randomNo = Math.floor(Math.random() * 16);
+          const newValue = character === 'x' ? randomNo : (randomNo & 0x3) | 0x8;
+
+          return newValue.toString(16);
+        });
+      }
+
+      const widget = new CryptoVoucherWidget({
+        selector: '#widget_embed',
+        token: '2k2KCY7PC2u2J9uv9BeTe4yN',
+        baseUrl: 'https://widget-stage.cryptovoucher.io'
+      });
+
+      const uid = uuid()
+      console.log('starting widget transaction | with uuid: ', uid)
+      console.log('amount | ', 25)
+      console.log('currency | ', 'EUR')
+      widget.start(25, 'EUR', uid);
+    },
+    referral() {
+      const widget = new CryptoVoucherWidget({
+        selector: '#widget_embed',
+        token: '2k2g257PC2u2J9uv9BeTe46U',
+        baseUrl: 'https://widget-stage.cryptovoucher.io'
+      });
+
+      console.log('starting widget referral transaction')
+      widget.start();
+    }
+  }
+}
+</script>
